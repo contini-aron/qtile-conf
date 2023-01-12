0914b9cae24619cd1bea84dc941bb6ad7beaeb45
contini-aron <contini.aron99@protonmail.com>
2023-01-12 20:26:22 +0000

    feat: added current local config

---
+++ autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+##################################
+# MONITOR SETUP
+##################################
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-3 --off --output DP-4 --off --output DP-5 --off
+##################################
+# BACKGROUND SETUP
+##################################
+feh --bg-fill ~/Downloads/wolf.jpg
+##################################
+# run picom
+##################################
+picom
